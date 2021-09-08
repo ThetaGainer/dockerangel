aad47acb3df9dafa6fa776226ad69b90e6636fe2
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-08 23:10:35 +0530

    Create common.py

---
+++ research/common.py
@@ -0,0 +1,41 @@
+ # Products
+    PRODUCT_MIS = "MIS"
+
+    PRODUCT_CNC = "CNC"
+    PRODUCT_NRML = "NRML"
+    PRODUCT_CO = "CO"
+    PRODUCT_BO = "BO"
+
+    # Order types
+    ORDER_TYPE_MARKET = "MARKET"
+    ORDER_TYPE_LIMIT = "LIMIT"
+    ORDER_TYPE_SLM = "SL-M"
+    ORDER_TYPE_SL = "SL"
+
+    # Varities
+    VARIETY_REGULAR = "regular"
+    VARIETY_BO = "bo"
+    VARIETY_CO = "co"
+    VARIETY_AMO = "amo"
+
+    # Transaction type
+    TRANSACTION_TYPE_BUY = "BUY"
+    TRANSACTION_TYPE_SELL = "SELL"
+
+    # Validity
+    VALIDITY_DAY = "DAY"
+    VALIDITY_IOC = "IOC"
+
+    # Exchanges
+    EXCHANGE_NSE = "NSE"
+    EXCHANGE_BSE = "BSE"
+    EXCHANGE_NFO = "NFO"
+    EXCHANGE_CDS = "CDS"
+    EXCHANGE_BFO = "BFO"
+    EXCHANGE_MCX = "MCX"
+    EXCHANGE_NCDEX="NCDEX"
+
+    # Status constants
+    STATUS_COMPLETE = "COMPLETE"
+    STATUS_REJECTED = "REJECTED"
+    STATUS_CANCELLED = "CANCELLED"
