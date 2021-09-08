868659a8fa665035a3c720c040fed15cff4a5773
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-08 17:26:03 +0530

    Create 2_orderplace.py

---
+++ research/2_orderplace.py
@@ -0,0 +1,22 @@
+from smartapi.SmartConnect import SmartConnect
+import pandas as pd
+import requests
+import login as l
+
+############################ Session Block #############
+#login api call
+obj=SmartConnect(api_key=l.api_key)
+data = obj.generateSession(l.user_name,l.password)
+refreshToken= data['data']['refreshToken']
+
+#fetch the feedtoken
+feedToken=obj.getfeedToken()
+l.feed_token = feedToken
+#fetch User Profile
+userProfile=obj.getProfile(refreshToken)
+print(userProfile)
+
+############################ End of Session Block #############
+
+############################ Order Placement #############
+
