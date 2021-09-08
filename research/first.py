8edfc72beff4ce8d21469a952f04e17dc1d9ff6e
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-08 17:13:52 +0530

    Update first.py

--- research/first.py
+++ research/first.py
@@ -1,1 +1,16 @@
-import
+from smartapi.SmartConnect import SmartConnect
+import pandas as pd
+import requests
+import login as l
+
+#login api call
+obj=SmartConnect(api_key=l.api_key)
+data = obj.generateSession(l.user_name,l.password)
+refreshToken= data['data']['refreshToken']
+
+#fetch the feedtoken
+feedToken=obj.getfeedToken()
+l.feed_token = feedToken
+#fetch User Profile
+userProfile=obj.getProfile(refreshToken)
+print(userProfile)
