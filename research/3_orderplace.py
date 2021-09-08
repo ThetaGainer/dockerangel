32292591528d8036bfb3fe5e8c89b7adeadc67ca
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-08 17:39:51 +0530

    Update 3_orderplace.py

--- research/3_orderplace.py
+++ research/3_orderplace.py
@@ -18,5 +18,34 @@ print(userProfile)
 
 ############################ End of Session Block #############
 
-############################ Order Placement #############
+############################ OHLC Block #############
 
+def OHLCHistory(symbol,token,interval,fdate,todate):
+    try:
+        historicParam={       
+        "exchange": "NSE",
+        "tradingsymbol":symbol,
+        "symboltoken": token,
+        "interval": interval,
+        "fromdate": fdate,
+        "todate": todate
+        }
+
+        history = obj.getCandleData(historicParam)['data']
+        history = pd.DataFrame(history)
+
+        history = history.rename(
+            columns={0: "Datetime", 1: "open", 2: "high", 3: "low", 4: "close", 5: "Volume"})
+
+        history['Datetime'] = pd.to_datetime(history['Datetime'])
+        history = history.set_index('Datetime')
+
+        return history
+    except Exception as e:
+        print("API failed: {}".format(e))
+   
+  ############################ End of OHLC Block #############
+  
+  ############################ Order Placement Block #############
+  
+  ############################ Endo of Order Placement Block #############
