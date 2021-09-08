a98ee1b35678cb4748fbd875d79398526aa04960
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-08 18:30:01 +0530

    Update 3_orderplace.py

--- research/3_orderplace.py
+++ research/3_orderplace.py
@@ -75,7 +75,7 @@ def place_order(symbol,token,qty,exch_seg,buy_sell,ordertype,price):
     except Exception as e:
         print("Order placement failed: {}".format(e))              
 
-print(place_order("SBIN-EQ", "3045",1,'NSE','BUY','MARKET,0'))
+print(place_order("SBIN-EQ", "3045",1,'NSE','BUY','MARKET,'0'))
 print("End Program")
 
 ############################ Endo of Order Placement Block #############
