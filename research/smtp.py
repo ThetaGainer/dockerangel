c955eddca995c9840c5f6738c2b0a9e243cb5129
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-09 00:26:03 +0530

    Update smtp.py

--- research/smtp.py
+++ research/smtp.py
@@ -1,26 +1,29 @@
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 import login as l
 
-mail_content = "Hello This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library"
-#The mail addresses and password
-sender_address = l.sender_email #sender_address = 'sender123@gmail.com' 
-sender_pass = l.sender_pass #sender_pass = 'xxxxxxxx' 
-receiver_address = l.receiver_email #receiver_address = 'receiver567@gmail.com' 
+gmail_user = l.g_user
+gmail_password = l.g_password
+receiver_user = l.r_user
 
-#Setup the MIME
-message = MIMEMultipart()
-message['From'] = sender_address
-message['To'] = receiver_address
-message['Subject'] = 'A test mail sent by Python. It has an attachment.'   #The subject line
-#The body and the attachments for the mail
-message.attach(MIMEText(mail_content, 'plain'))
-#Create SMTP session for sending the mail
-session = smtplib.SMTP('smtp.gmail.com', 587) #use gmail with port
-session.starttls() #enable security
-session.login(sender_address, sender_pass) #login with mail_id and password
-text = message.as_string()
-session.sendmail(sender_address, receiver_address, text)
-session.quit()
-print('Mail Sent')
+sent_from = gmail_user
+to = [receiver_user, 'scalper.niftybanknifty@gmail.com']
+subject = 'Alert'
+body = 'Hey, whats up'
+
+email_text = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (sent_from, ", ".join(to), subject, body)
+
+try:
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    server.ehlo()
+    server.login(gmail_user, gmail_password)
+    server.sendmail(sent_from, to, email_text)
+    server.close()
+    print ('Email sent!')
+except:
+    print ('Something went wrong...again')
