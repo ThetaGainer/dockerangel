6eb41f8c5f5064dd25adcd74f63aa6c1f3333584
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-08 18:21:12 +0530

    Update 3_orderplace.py

--- research/3_orderplace.py
+++ research/3_orderplace.py
@@ -57,19 +57,19 @@ print(my_df)
 def place_order(symbol,token,qty,exch_seg,buy_sell,ordertype,price):
     try:
         orderparams = {
-              "variety":"NORMAL",
-              "tradingsymbol":symbol,
-              "symboltoken":token,
-              "transactiontype":buy_sell,
-              "exchange":exch_seg,
-              "ordertype":ordertype,
-              "producttype":"INTRADAY",
-              "duration":"DAY",
-              "price":price,
-              "squareoff":"0",
-              "stoploss":"0",
-              "quantity":qty
-              }
+            "variety":"NORMAL",
+            "tradingsymbol":symbol,
+            "symboltoken":token,
+            "transactiontype":buy_sell,
+            "exchange":exch_seg,
+            "ordertype":ordertype,
+            "producttype":"INTRADAY",
+            "duration":"DAY",
+            "price":price,
+            "squareoff":"0",
+            "stoploss":"0",
+            "quantity":qty
+            }
         orderId=obj.placeOrder(orderparams)
         print("The order id is: {}".format(orderId))
     except Exception as e:
