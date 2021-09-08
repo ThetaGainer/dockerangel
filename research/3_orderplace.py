09e9c3a2079a71c593a0a0ee9387d55d3b1efd0e
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-08 17:49:26 +0530

    Update 3_orderplace.py

--- research/3_orderplace.py
+++ research/3_orderplace.py
@@ -47,5 +47,25 @@ def OHLCHistory(symbol,token,interval,fdate,todate):
   ############################ End of OHLC Block #############
   
   ############################ Order Placement Block #############
-  
+
+  def place_order(symbol,token,qty,exch_seg,buy_sell,ordertype,price):
+      try:
+          orderparams = {
+              "variety":"NORMAL",
+              "tradingsymbol":symbol,
+              "symboltoken":token,
+              "transactiontype":buy_sell,
+              "exchange":exch_seg,
+              "ordertype":ordertype,
+              "producttype":"INTRADAY",
+              "duration":"DAY",
+              "price":price,
+              "squareoff":"0",
+              "stoploss":"0",
+              "quantity":qty
+              }
+            orderId=obj.placeOrder(orderparams)
+            print("The order id is: {}".format(orderId))
+        except Exception as e:
+            print("Order placement failed: {}".format(e))              
   ############################ Endo of Order Placement Block #############
