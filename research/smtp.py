bbd48b5e2d1125c0e4a0a4ba2a505b650aae27fb
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-08 23:22:58 +0530

    Update smtp.py

--- research/smtp.py
+++ research/smtp.py
@@ -1,14 +1,16 @@
 import smtplib
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
+import login as l
+
 mail_content = '''Hello,
 This is a simple mail. There is only text, no attachments are there The mail is sent using Python SMTP library.
 Thank You
 ' ' '
 #The mail addresses and password
-sender_address = 'sender123@gmail.com'
-sender_pass = 'xxxxxxxx'
-receiver_address = 'receiver567@gmail.com'
+sender_address = 'sender123@gmail.com' #sender_address = l.sender_email
+sender_pass = 'xxxxxxxx' #sender_pass = l.sender_pass
+receiver_address = 'receiver567@gmail.com' #receiver_address = l.receiver_email
 #Setup the MIME
 message = MIMEMultipart()
 message['From'] = sender_address
