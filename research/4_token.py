14b022ea67bf97b4b2522f00162494c1fb9e807d
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2021-09-09 15:27:51 +0530

    Update 4_token.py

--- research/4_token.py
+++ research/4_token.py
@@ -21,6 +21,39 @@ print(userProfile)
 
 ############################ End of Session Block #############
 
+############################ OHLC Block #############
+
+def OHLCHistory(symbol,token,interval,fdate,todate):
+    try:
+        historicParam={       
+        "exchange": "NSE",
+        "tradingsymbol":symbol,
+        "symboltoken": token,
+        "interval": interval,
+        "fromdate": fdate,
+        "todate": todate
+        }
+
+        history = obj.getCandleData(historicParam)['data']
+        history = pd.DataFrame(history)
+
+        history = history.rename(
+            columns={0: "Datetime", 1: "open", 2: "high", 3: "low", 4: "close", 5: "Volume"})
+
+        history['Datetime'] = pd.to_datetime(history['Datetime'])
+        history = history.set_index('Datetime')
+
+        return history
+    except Exception as e:
+        print("API failed: {}".format(e))
+
+minute5data = OHLCHistory("SBIN-EQ", "3045", "FIVE_MINUTE", "2021-06-03 00:00", "2021-06-04 15:30")
+print("5 minute data Live:")
+my_df = pd.DataFrame(minute5data)
+print(my_df)
+
+
+############################ End of OHLC Block #############
 ############################ Symbol Mapping Block #############
 
 Dailydata = {}
@@ -63,39 +96,7 @@ print("End of Algo")
 
 
 
-############################ OHLC Block #############
 
-def OHLCHistory(symbol,token,interval,fdate,todate):
-    try:
-        historicParam={       
-        "exchange": "NSE",
-        "tradingsymbol":symbol,
-        "symboltoken": token,
-        "interval": interval,
-        "fromdate": fdate,
-        "todate": todate
-        }
-
-        history = obj.getCandleData(historicParam)['data']
-        history = pd.DataFrame(history)
-
-        history = history.rename(
-            columns={0: "Datetime", 1: "open", 2: "high", 3: "low", 4: "close", 5: "Volume"})
-
-        history['Datetime'] = pd.to_datetime(history['Datetime'])
-        history = history.set_index('Datetime')
-
-        return history
-    except Exception as e:
-        print("API failed: {}".format(e))
-
-minute5data = OHLCHistory("SBIN-EQ", "3045", "FIVE_MINUTE", "2021-06-03 00:00", "2021-06-04 15:30")
-print("5 minute data Live:")
-my_df = pd.DataFrame(minute5data)
-print(my_df)
-
-
-############################ End of OHLC Block #############
   
 ############################ Order Placement Block #############
 """
