e82e0fea07c023cd5206f76652e782a1dab0e262
ThetaGainer <90305147+ThetaGainer@users.noreply.github.com>
2022-10-23 13:20:36 +0530

    TOTP
    TOTP

--- research/1_login.py
+++ research/1_login.py
@@ -1,19 +1,18 @@
 from smartapi import SmartConnect #from smartapi.SmartConnect import SmartConnect
-import pandas as pd
-import requests
 import login as l
+import pyotp
 
 ############################ Session Block #############
 #login api call
 obj=SmartConnect(api_key=l.api_key)
-data = obj.generateSession(l.user_name,l.password)
+data = obj.generateSession(l.user_name,l.password,pyotp.TOTP(l.code).now())
 refreshToken= data['data']['refreshToken']
 
 #fetch the feedtoken
 feedToken=obj.getfeedToken()
-l.feed_token = feedToken
+#l.feed_token = feedToken
 #fetch User Profile
 userProfile=obj.getProfile(refreshToken)
-print(userProfile)
+#print(userProfile)
 
 ############################ End of Session Block #############
